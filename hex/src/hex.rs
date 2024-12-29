f4b5173f22e356290b14e90344c38ecb68ae71a5
Mark Lamourine <markllama@gmail.com>
2024-12-28 19:48:13 -0500

    added sub classes for maps

--- hex/src/hex.rs
+++ hex/src/hex.rs
@@ -9,3 +9,17 @@ pub struct Hex {
 //    contents: mut Vec<Item>,
 //    occupants: mut Vec<Unit>	
 }
+
+impl Hex {
+    pub fn new(location: Point) -> Hex {
+	Hex { location: location }
+    }
+}
+#[cfg(test)]
+
+#[test]
+fn test_hex() {
+    let h1 = Hex { location: Point { hx: -3, hy: 14 } };
+
+    assert_eq!( h1.location.hx, -3 );
+}
--- hex/src/map.rs
+++ hex/src/map.rs
@@ -1,12 +1,16 @@
 //
 // 
 //
-use crate::point::Point;
+use std::ops::Index;
+
+use crate::point::{Point,ORIGIN};
+use crate::hex::Hex;
+
 
 pub enum Shape {
     Rectangle,
     Hexagon,
-    MetaHex
+    MegaHex
 }
 
 pub enum Orientation {
@@ -14,8 +18,58 @@ pub enum Orientation {
     Vertical
 }
 
+pub struct Row {
+    row: Vec<Hex>
+}
+
+impl Row {
+    fn New() -> Row {
+	Row { row: vec!() }
+    }
+
+    fn push(&mut self, h: Hex) {
+	self.row.push(h);
+    }
+}
+
+impl Index<usize> for Row {
+    type Output = Hex;
+
+    fn index(&self, loc: usize) -> &Hex {
+	&(self.row[loc])
+    }
+
+}
+
 pub struct Map {
-    shape: Shape,
-    orientation: Orientation,
-    size: Point    
+    pub shape: Shape,
+    pub orientation: Orientation,
+    pub size: Point,
+    pub origin: Point,
+    rows: Vec<Row>
+}
+
+impl Map {
+    
+    fn New(shape: Shape, orientation: Orientation, size: Point, origin: Point) -> Map {
+	let mut rows: Vec<Row> = vec!();
+
+	for i in 0..size.hx {
+	    rows.push(Row::New());
+	    for j in 0..size.hy {
+		rows[i as usize].push(Hex::new (Point { hx: i + origin.hx, hy: j + origin.hy }));
+	    }
+	}
+	
+	Map { shape: shape, orientation: orientation, size: size, origin: origin, rows: rows }
+    }
+}
+
+#[cfg(test)]
+
+#[test]
+fn test_map_new() {
+    let m0 = Map::New(Shape::Rectangle, Orientation::Vertical, Point { hx: 5, hy: 6 }, ORIGIN);
+
+    assert_eq!(m0.size.hx, 5);
 }
